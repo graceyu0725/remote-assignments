c4eadc1705d6832e5e2790d104689741f82fc7c3
Grace Yu <grace.yupt@gmail.com>
2023-09-09 02:43:35 +0800

    update week-2 assignment

--- week-2/README.md
+++ week-2/README.md
@@ -2,6 +2,7 @@
 
 - assignment-1: Function and Array  
   [GitHub Page](https://graceyu0725.github.io/remote-assignments/week-2/assignment-1)
+  
 - assignment-2: Function and Object  
   [GitHub Page](https://graceyu0725.github.io/remote-assignments/week-2/assignment-2)
 
--- week-2/assignment-4/js/app.js
+++ week-2/assignment-4/js/app.js
@@ -4,8 +4,15 @@ When the user clicks on the "Welcome Message" block,
 change text to "Have a Good Time!".
 */
 
+let changeTitle = document.querySelector(".title");
 
-
+changeTitle.addEventListener("click", () => {
+  if (changeTitle.textContent === "Welcome Message") {
+    changeTitle.textContent = "Have a Good Time!";
+  } else if (changeTitle.textContent === "Have a Good Time!") {
+    changeTitle.textContent = "Welcome Message";
+  }
+});
 
 /*
 Request 2: Click to Show/Close Menu.
@@ -13,6 +20,17 @@ When the user clicks the menu at the top-right corner,
 show the hidden mobile menu. After that, the user can click the close button to hide it.
 */
 
+let openMenu = document.querySelector(".menu");
+let mobileMenu = document.querySelector(".mobile-menu");
+let closeMenu = document.querySelector(".close-button");
+
+openMenu.addEventListener("click", () => {
+  mobileMenu.style.display = "block";
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.style.display = "none";
+});
 
 /*
 Request 3: Click to Show More Content Boxes.
@@ -20,3 +38,16 @@ There are some more content boxes waiting to show.
 When the user clicks the Call-to-Action button, 
 show those hidden content boxes.
 */
+
+let showMore = document.querySelector(".cta");
+let moreBoxes = document.querySelector(".more-boxes");
+
+showMore.addEventListener("click", () => {
+  if (showMore.textContent === "Show More") {
+    moreBoxes.style.display = "block";
+    showMore.textContent = "Show Less";
+  } else if (showMore.textContent === "Show Less") {
+    moreBoxes.style.display = "none";
+    showMore.textContent = "Show More";
+  }
+});
