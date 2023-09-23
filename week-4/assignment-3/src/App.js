1ee332a9095bc4a87b1304789247595d10256568
Grace Yu <grace.yupt@gmail.com>
2023-09-23 14:06:30 +0800

    update week-4 assignment

--- week-4/assignment-3/src/App.js
+++ week-4/assignment-3/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { HashRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
@@ -6,11 +8,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Header />
       <Main />
       <Footer />
-    </>
+    </HashRouter>
   );
 }
 
--- week-4/assignment-3/src/index.js
+++ week-4/assignment-3/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
-<BrowerserRouter basename="week-4/assignment-3/public">
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
   );
-</BrowerserRouter>;
