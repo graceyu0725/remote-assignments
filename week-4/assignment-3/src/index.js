08a4abe460298441e9a8542396cc9885cbf75ada
Grace Yu <grace.yupt@gmail.com>
2023-09-23 13:44:14 +0800

    update week-4 assignment

--- week-4/assignment-3/src/index.js
+++ week-4/assignment-3/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+<BrowerserRouter basename="week-4/assignment-3/public">
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-
+  );
+</BrowerserRouter>;
